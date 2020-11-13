f11eff49fdb64bb4d466417ae030164bb15ee535
Tinh Nguyen Xuan <xngtinh@gmail.com>
2020-11-13 11:40:47 +0700

    Upload model

--- script.js
+++ script.js
@@ -1,86 +1,89 @@
-const canvas = document.getElementById('main-canvas');
-const smallCanvas = document.getElementById('small-canvas');
-const displayBox = document.getElementById('prediction');
 
-const inputBox = canvas.getContext('2d');
-const smBox = smallCanvas.getContext('2d');
 
-let isDrawing = false;
-let model;
+// Số thứ 2
+const canvas_second = document.getElementById('second-canvas');
+const smallCanvas_second = document.getElementById('second-small-canvas');
+const displayBox_second = document.getElementById('second-prediction');
+
+const inputBox_second = canvas_second.getContext('2d');
+const smBox_second = smallCanvas_second.getContext('2d');
+
+let isDrawing_second = false;
+let model_second;
 
 /* Loads trained model */
-async function init() {
-  model = await tf.loadModel('https://xngtinh.github.io/model/model.json');
+async function init_second() {
+  model_second = await tf.loadModel('https://xngtinh.github.io/model/model.json');
 }
 
-canvas.addEventListener('mousedown', event => {
-  isDrawing = true;
+canvas_second.addEventListener('mousedown', event => {
+  isDrawing_second = true;
 
-  inputBox.strokeStyle = 'white';
-  inputBox.lineWidth = '15';
-  inputBox.lineJoin = inputBox.lineCap = 'round';
-  inputBox.beginPath();
+  inputBox_second.strokeStyle = 'white';
+  inputBox_second.lineWidth = '15';
+  inputBox_second.lineJoin = inputBox_second.lineCap = 'round';
+  inputBox_second.beginPath();
 });
 
-canvas.addEventListener('mousemove', event => {
-  if (isDrawing) drawStroke(event.clientX, event.clientY);
+canvas_second.addEventListener('mousemove', event => {
+  if (isDrawing_second) drawStroke_second(event.clientX, event.clientY);
 });
 
-canvas.addEventListener('mouseup', event => {
-  isDrawing = false;
-  updateDisplay(predict());
+canvas_second.addEventListener('mouseup', event => {
+  isDrawing_second = false;
+  updateDisplay_second(predict_second());
 });
 
 /* Draws on canvas */
-function drawStroke(clientX, clientY) {
+function drawStroke_second(clientX_second, clientY_second) {
   // get mouse coordinates on canvas
-  const rect = canvas.getBoundingClientRect();
-  const x = clientX - rect.left;
-  const y = clientY - rect.top;
+  const rect_second = canvas_second.getBoundingClientRect();
+  const x_second = clientX_second - rect_second.left;
+  const y_second = clientY_second - rect_second.top;
 
   // draw
-  inputBox.lineTo(x, y);
-  inputBox.stroke();
-  inputBox.moveTo(x, y);
+  inputBox_second.lineTo(x_second, y_second);
+  inputBox_second.stroke();
+  inputBox_second.moveTo(x_second, y_second);
 }
 
 /* Makes predictions */
-function predict() {
-  let values = getPixelData();
-  let predictions = model.predict(values).dataSync();
+function predict_second() {
+  let values_second = getPixelData_second();
+  let predictions_second = model_second.predict_second(values_second).dataSync();
 
-  return predictions;
+  return predictions_second;
 }
 
 /* Returns pixel data from canvas after applying transformations */
-function getPixelData() {
-  smBox.drawImage(inputBox.canvas, 0, 0, smallCanvas.width, smallCanvas.height);
-  const imgData = smBox.getImageData(0, 0, smallCanvas.width, smallCanvas.height);
+function getPixelData_second() {
+  smBox_second.drawImage(inputBox_second.canvas_second, 0, 0, smallCanvas_second.width, smallCanvas_second.height);
+  const imgData_second = smBox_second.getImageData(0, 0, smallCanvas_second.width, smallCanvas_second.height);
 
   // preserve and normalize values from red channel only
-  let values = [];
-  for (let i = 0; i < imgData.data.length; i += 4) {
-    values.push(imgData.data[i] / 255);
+  let values_second = [];
+  for (let i = 0; i < imgData_second.data.length; i += 4) {
+    values_second.push(imgData_second.data[i] / 255);
   }
-  values = tf.reshape(values, [1, 28, 28, 1]);
-  return values;
+  values_second = tf.reshape(values_second, [1, 28, 28, 1]);
+  return values_second;
 }
 
 /* Displays predictions on screen */
-function updateDisplay(predictions) {
+function updateDisplay_second(predictions_second) {
   // Find index of best prediction, which corresponds to the predicted value
-  const bestPred = predictions.indexOf(Math.max(...predictions));
-  displayBox.innerText = bestPred;
+  const bestPred_second = predictions_second.indexOf(Math.max(...predictions_second));
+  displayBox_second.innerText = bestPred_second;
 }
 
-document.getElementById('erase').addEventListener('click', erase);
+document.getElementById('second-erase').addEventListener('click', erase_second);
 
 /* Clears canvas */
-function erase() {
-  inputBox.fillStyle = 'black';
-  inputBox.fillRect(0, 0, canvas.width, canvas.height);
-  displayBox.innerText = '';
+function erase_second() {
+  inputBox_second.fillStyle = 'black';
+  inputBox_second.fillRect(0, 0, canvas_second.width, canvas_second.height);
+  displayBox_second.innerText = '';
 }
 
-erase();
-init();
+erase_second();
+init_second();
